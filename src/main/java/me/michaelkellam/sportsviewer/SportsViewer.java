2e1daf7405d76bad45a519ca119d44b902d32bd2
Michael Kellam <michaelkellamcs@gmail.com>
2022-02-10 23:01:11 -0500

    First commit

---
+++ src/main/java/me/michaelkellam/sportsviewer/SportsViewer.java
@@ -0,0 +1,17 @@
+package me.michaelkellam.sportsviewer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SportsViewer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
